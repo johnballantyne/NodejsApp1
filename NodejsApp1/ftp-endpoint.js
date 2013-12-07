4bbd5ed7e02a37500d452691e55d262af01c82a5
Mikuso <g_github@flamescape.com>
2013-12-07 08:19:21 +0000

    More FTP stuff

--- NodejsApp1/ftp-endpoint.js
+++ NodejsApp1/ftp-endpoint.js
@@ -1,29 +1,93 @@
 var net = require('net');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
+var _ = require('underscore');
 require('colors');
 
-var PASV = function(cb) {
+var PASV = function(options, cb) {
+    options = _.extend({
+        timeout         : 1000 * 5, // 5 seconds should be long enough to establish ANY connection, right?
+        localAddress    : '127.0.0.1',
+        remoteAddress   : '0.0.0.0',
+        transferType    : 'I',
+        writeTo         : null  // this can be a writable stream, like a file stream.
+                                // if empty, will emit "data" event containing in-memory buffer
+    }, options || {});
+    
     var pasv = new EventEmitter();
+    
+    // if we specify a write stream before a client connects to the PASV port,
+    // we should be able to change the data destination.
+    pasv.pipe = function(stream){
+        return options.writeTo = stream;
+    };
 
+    var establishingTimeout;
+    
     var server = net.createServer(function(socket){
-        console.log('PASV connection received');
+    
+        if (socket.remoteAddress != options.remoteAddress) {
+            // prevent PASV connection theft
+            // http://cr.yp.to/ftp/security.html
+            socket.destroy();
+            return;
+        }
+    
+        // Connection accepted; stop accepting more connections on this port
+        server.close();
+        clearTimeout(establishingTimeout);
         
         pasv.emit('connect', socket);
         
-        socket.on("data", function(data){
-            console.log('PASVDATA', data);
+        if (options.writeTo) {
+            // pipe data directly into our writable stream
+            socket.pipe(options.writeTo);
+        } else {
+            // if we don't have anywhere to write the data, buffer it and emit an event on end
+            var readBuffer = new Buffer(0);
+            
+            socket.on('data', function(data){
+                // add data to our read buffer
+                readBuffer = Buffer.concat([readBuffer, data]);
+            });
+            
+            // socket closed by the other end
+            socket.once('end', function(){
+                socket.removeAllListeners('data');
+                // if readBuffer contains data, notify anyone who cares
+                if (readBuffer.length) {
+                    pasv.emit('data', readBuffer);
+                }
+            });
+        }
+        
+        socket.once('close', function(){
+            pasv.emit('close');
         });
+        
+        socket.once('error', function(){
+            socket.destroy();
+        });
+        
+        socket.once('timeout', function(){
+            socket.end();
+        });
+        
+        socket.setTimeout(1000 * 30); // this is the data transfer timeout, not the connection establishing timeout
     });
+    
+    // THIS is the connection establishing timeout
+    establishingTimeout = setTimeout(function(){
+        pasv.emit('timeout');
+        server.close();
+        server.removeAllListeners();
+        server = null;
+    }, options.timeout);
     
     server.listen(function(){
         var a = server.address();
         
-        if (a.address === '0.0.0.0') {
-            a.address = '127.0.0.1';
-        }
-        
-        var address = a.address.split('.');
+        var address = options.localAddress.split('.');
         address.push((a.port / 256) << 0);
         address.push(a.port % 256);
         
@@ -41,6 +105,8 @@ var createControlLink = function(port, newClientCallback){
           , paused = false
           , dataLink = null
           ;
+
+        client.socket = socket;
 
         // receive a command from the client if ready
         client.receive = function(){
@@ -70,7 +136,6 @@ var createControlLink = function(port, newClientCallback){
         };
         // resume accepting commands on the control link
         client.resume = function(){
-            console.log('resuming', readBuffer);
             paused = false;
             client.emit('resume');
             client.receive();
@@ -129,73 +194,138 @@ var createControlLink = function(port, newClientCallback){
 
 };
 
-createControlLink(21, function(client){
-    var dataLink = null;
+var VirtualFTPServer = function(serverWelcome, listenPort, options) {
 
-    client.on('command', function(command){
-        switch (command.cmd) {
-            // useful info:
-            // http://www.ietf.org/rfc/rfc959.txt
-            // http://en.wikipedia.org/wiki/List_of_FTP_server_return_codes
-            case 'USER': // Here's a username
-                //if (msg.param == 'theusername')
-                client.send(331, "Password required");
-                break;
-            case 'PASS': // Here's a password
-                //if (msg.param == 'thepassword')
-                client.send(230, "User logged in");
-                break;
-            case 'PWD': // What is the present working directory?
-                client.send(257, "\"/\" is current directory");
-                break;
-            case 'TYPE': // Set TYPE
-                client.send(200, "Type set to "+command.param);
-                break;
-            case 'PASV': // Enter Passive mode?
-                client.pause();
-                PASV(function(err, pasv){
-                    if (err) {
-                        client.send(421, "Couldn't create data link");
-                        client.resume();
+    var vftp = this;
+
+    createControlLink(listenPort || 21, function(client){
+        var dataLink        = null;
+        var transferType    = "L8";
+        var authedUser      = null; // user name authenticated by system
+        var namedUser       = null; // user name specified in last USER command
+
+        client.on('command', function(command){
+            switch (command.cmd) {
+                // useful info:
+                // http://cr.yp.to/ftp/security.html
+                // http://www.ietf.org/rfc/rfc959.txt
+                // http://en.wikipedia.org/wiki/List_of_FTP_server_return_codes
+                case 'USER': // Here's a username
+                    namedUser = command.param;
+                    client.send(331, "Password required");
+                    break;
+                case 'PASS': // Here's a password
+                    if (!namedUser) return client.send(503, "Need username first");
+                    vftp.emit("auth", namedUser, command.param, _.once(function(err){
+                        if (err) {
+                            namedUser = null;
+                            return client.send(530, "Not authorised");
+                        }
+                        
+                        authedUser = namedUser;
+                        client.send(230, "User logged in");
+                    }));
+                    break;
+                case 'PWD': // What is the present working directory?
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    client.send(257, "\"/\" is current directory");
+                    break;
+                case 'TYPE': // Set TYPE
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    transferType = command.param;
+                    client.send(200, "Type set to "+transferType);
+                    break;
+                case 'PASV': // Enter Passive mode?
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    client.pause();
+                    PASV({
+                        transferType    : transferType,
+                        localAddress    : client.socket.localAddress,
+                        remoteAddress   : client.socket.remoteAddress
+                    }, _.once(function(err, pasv){
+                        if (err) {
+                            client.send(421, "Couldn't create data link");
+                            client.resume();
+                        }
+                        
+                        pasv.once('connect', function(){
+                            dataLink = pasv;
+                            client.resume();
+                        });
+                        
+                        client.send(227, "Entering Passive Mode ("+pasv.address+").");
+                        
+                        pasv.once('timeout', function(){
+                            // timed out waiting for client to connect to our PASV server
+                            pasv.removeAllListeners('connect');
+                            client.resume();
+                        });
+                    }));
+                    break;
+                case 'NOOP': // No operation
+                case 'CLNT': // Client is trying to tell us what agent they're using
+                case 'QUIT': // Client is signing off
+                    client.send(200, "That's fine, whatever");
+                    break;
+                case 'LIST':
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    if (!dataLink) {
+                        client.send(503, "Must establish data link first");
+                        break;
                     }
                     
-                    pasv.once('connect', function(dlClient){
-                        dataLink = dlClient;
-                        client.resume();
-                    });
-                    
-                    client.send(227, "Entering Passive Mode ("+pasv.address+").");
-                });
-                break;
-            case 'NOOP': // No operation
-            case 'CLNT': // Client is trying to tell us what agent they're using
-            case 'QUIT': // Client is signing off
-                client.send(200, "That's fine, whatever");
-                break;
-            case 'LIST':
-                if (!dataLink) {
-                    client.send(503, "Must establish data link first");
+                    client.send(150, "Starting transfer");
+                    dataLink.socket.end("drwxr-xr-x   3 (?)      (?)          2048 Aug 23  2007 P25 Compact Series\r\n");
+                    client.send(226, "Transfer complete");
                     break;
-                }
-                
-                client.send(150, "Starting transfer");
-                dataLink.write("drwxr-xr-x   3 (?)      (?)          2048 Aug 23  2007 P25 Compact Series\r\n");
-                client.send(226, "Transfer complete");
-                break;
-            case 'SIZE': // What size is this file?
-                client.send(213, 0);
-                break;
-            case 'CWD': // Change working directory
-                client.send(250, "Sure, that's fine");
-                break;
-            case 'FEAT':
-                client.send(211, ["Features:\r\n NLST\r\n PASV", "End"]);
-                break;
-            default: // we don't understand the command
-                client.send(502, "Command not implemented");
-                break;
-        }
+                case 'SIZE': // What size is this file?
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    client.send(213, 0);
+                    break;
+                case 'CWD': // Change working directory
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    client.send(250, "Sure, that's fine");
+                    break;
+                case 'FEAT': // What features does this FTP server have?
+                    client.send(211, ["Features:\r\n NLST\r\n PASV", "End"]);
+                    break;
+                case 'SYST': // What system is this FTP server running on?
+                    client.send(215, "UNIX Type: "+transferType);
+                    break;
+                case 'STOR': // Client wants to send us a file
+                    if (!authedUser) return client.send(530, "Not authorised");
+                    if (!dataLink) {
+                        client.send(503, "Must establish data link first");
+                        break;
+                    }
+                    
+                    client.send(150, "Start transferring");
+                    dataLink.once('data', function(data) {
+                        console.log('PASV received: ', data);
+                        client.send(226, "Transfer complete");
+                    });
+                    break;
+                case 'DELE': // Client wants to delete a file
+                    client.send(250, "Deleted file");
+                    break;
+                default: // we don't understand the command
+                    client.send(502, "Command not implemented");
+                    break;
+            }
+        });
+
+        client.send(220, [serverWelcome, "FTP Control link ready"]);
     });
 
-    client.send(220, ["Gareth's Node FTP server", "FTP Control link ready"]);
+};
+VirtualFTPServer.prototype.__proto__ = EventEmitter.prototype; // inherit from EventEmitter
+    
+
+var ftp = new VirtualFTPServer("Race07 Live-FTP Endpoint", 21, {debug: true});
+ftp.on('auth', function(username, password, cb){
+    console.log('u/p:', username, password);
+    return cb&&cb();
+});
+ftp.on('stor', function(filename, action){
+
 });
