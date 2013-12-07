b4b76a0a42d35fa1ad6eaa09115807f0ceb95b4f
Mikuso <g_github@flamescape.com>
2013-12-07 05:08:54 +0000

    Track images displayed

--- NodejsApp1/index.js
+++ NodejsApp1/index.js
@@ -1,16 +1,15 @@
 ﻿var fs = require('fs');
 var parse = require('./ParserStuff/parser');
+var express = require('express.io');
 
 var fileString = fs.readFileSync('bleh.txt', {encoding:'utf8'});
 var race = parse(fileString);
 
-var app = require('express.io')();
+var app = express();
 app.http().io();
 app.listen(8888);
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname));
 
 app.io.sockets.on('connection', function (socket) {
     socket.emit('race07 data',race);
